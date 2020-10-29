93688df5147be58b6a6537ed35a48d056f9210b0
OrKatz7 <Or.Katz1@gmail.com>
2020-10-29 09:15:56 +0200

    Update train.sh

--- cnn2d/train.sh
+++ cnn2d/train.sh
@@ -1,16 +1,16 @@
+python3 preprocessing.py
+
 python3 train.py --config efficientnetb3 --fold 0 --folds 5
 python3 train.py --config efficientnetb3 --fold 1 --folds 5
 python3 train.py --config efficientnetb3 --fold 2 --folds 5
 python3 train.py --config efficientnetb3 --fold 3 --folds 5
 python3 train.py --config efficientnetb3 --fold 4 --folds 5
 
-
 python3 train.py --config efficientnetb4 --fold 0 --folds 5
 python3 train.py --config efficientnetb4 --fold 1 --folds 5
 python3 train.py --config efficientnetb4 --fold 2 --folds 5
 python3 train.py --config efficientnetb4 --fold 3 --folds 5
 python3 train.py --config efficientnetb4 --fold 4 --folds 5
-
 
 python3 train.py --config efficientnetb5 --fold 0 --folds 5
 python3 train.py --config efficientnetb5 --fold 1 --folds 5
